f783711c64daff04c8abdb6424ecbb9ea7f14428
NKerner <nickkerner59@gmail.com>
2019-08-14 10:45:30 -0400

    corrected error

--- objectPairedDrills.js
+++ objectPairedDrills.js
@@ -1,6 +1,6 @@
 function createMyObject(){
     
-    let myObj = {foo: 'bar',answerToUniverse: '42', ollyolly: 'oxen free',}
+    let myObj = {foo: 'bar',answerToUniverse: '42', ['olly olly']: 'oxen free',}
     myObj.hello = function(){
         return 'hello' 
     }
