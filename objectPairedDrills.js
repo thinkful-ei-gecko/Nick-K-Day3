2ae051e98dd0c209c13f5cc6c363ee9429fa8261
William Bae <wbae912@gmail.com>
2019-08-14 09:50:23 -0700

    made file cleaner

--- objectPairedDrills.js
+++ objectPairedDrills.js
@@ -1,133 +1,136 @@
 'use strict';
 
-/*function createMyObject(){
-  let myObj = {foo: 'bar',answerToUniverse: '42', ['olly olly']: 'oxen free',}
-  myObj.hello = function(){
-    return 'hello' 
-  }
-  return myObj;
-}
+// function createMyObject(){
+//   let myObj = {foo: 'bar',answerToUniverse: '42', ['olly olly']: 'oxen free',}
+//   myObj.hello = function(){
+//     return 'hello' 
+//   }
+//   return myObj;
+// }
+// ////////////////
+// function updateObject(obj){
+//   obj.foo = 'foo';
+//   obj.bar ='bar';
+//   obj.bizz ='bizz';
+//   obj.bang = 'bang';
+//   return obj;
+// }
+// ////////////////
+// function personMaker(){
+//   var person ={
+//     firstName: 'Nick',
+//     lastName: 'Kerner',
+//     fullName: function(){
+//       return this.firstName + ' ' + this.lastName;
+//     },
+//   };
+// }
+// ////////////////
+// const sampleObj = {
+//   foo: 'foo',
+//   bar: 'bar',
+//   bizz: 'bizz',
+//   bang: 'bang',
+// };
 
-function updateObject(obj){
-  obj.foo = 'foo';
-  obj.bar ='bar';
-  obj.bizz ='bizz';
-  obj.bang = 'bang';
-  return obj;
-}
+// function keyDeleter(obj) {
+//   delete obj.foo;
+//   delete obj.bar;
+//   return obj;
+// }
+// ////////////////
+// function makeStudentsReport(data) {
+//   let result = [];
+//   for (let i = 0; i < data.length; i++) {
+//     result.push(`${data[i].name}: ${data[i].grade}`);
+//   }
+//   return result;
+// }
 
-function personMaker(){
-  var person ={
-    firstName: 'Nick',
-    lastName: 'Kerner',
-    fullName: function(){
-      return this.firstName + ' ' + this.lastName;
-    },
-  };
-}
-
-const sampleObj = {
-  foo: 'foo',
-  bar: 'bar',
-  bizz: 'bizz',
-  bang: 'bang',
-};
+// const example = [{name: 'Johnny Robot', grade: 'C'}];
+// console.log(makeStudentsReport(example));*/
+// ////////////////
+// const studentData = [
+//   {
+//     name: 'Tim',
+//     status: 'Current student',
+//     course: 'Biology',
+//   },
+//   {
+//     name: 'Sue',
+//     status: 'Withdrawn',
+//     course: 'Mathematics',
+//   },
+//   {
+//     name: 'Liz',
+//     status: 'On leave',
+//     course: 'Computer science',
+//   },
+// ];
   
-function keyDeleter(obj) {
-  delete obj.foo;
-  delete obj.bar;
-  return obj;
-}*/
+// function enrollInSummerSchool(students) {
+//   for (let i = 0; i < students.length; i++) {
+//     students[i].status = 'In summer school';
+//   }
+//   return students;
+// }
 
-/*function makeStudentsReport(data) {
-  let result = [];
-  for (let i = 0; i < data.length; i++) {
-    result.push(`${data[i].name}: ${data[i].grade}`);
-  }
-  return result;
-}
+// let input = [
+//   {
+//     name: 'Tim',
+//     status: 'Current student',
+//     course: 'Biology'
+//   },
+//   {
+//     name: 'Sue',
+//     status: 'Withdrawn',
+//     course: 'Mathematics'
+//   }
+// ];
 
-const example = [{name: 'Johnny Robot', grade: 'C'}];
-console.log(makeStudentsReport(example));*/
+// console.log(enrollInSummerSchool(input));
+// ////////////////
 
-/*const studentData = [
-  {
-    name: 'Tim',
-    status: 'Current student',
-    course: 'Biology',
-  },
-  {
-    name: 'Sue',
-    status: 'Withdrawn',
-    course: 'Mathematics',
-  },
-  {
-    name: 'Liz',
-    status: 'On leave',
-    course: 'Computer science',
-  },
-];
-  
-function enrollInSummerSchool(students) {
-  for (let i = 0; i < students.length; i++) {
-    students[i].status = 'In summer school';
-  }
-  return students;
-}
+// function findById(items,idNum){
+//   for(let i = 0;i < items.length; i++){
+//     if(items[i].id === idNum){
+//       return items[i]
+//     }
+//   }
+// }
+// ////////////////
+// function validateKeys(object,expectedKeys){
+//   let keys = Object.keys(object);
+//   for(let i = 0; i < keys.length; i++) {
+//     if(keys[i] === expectedKeys[i] && keys.length === expectedKeys.length){
+//       return true;
+//       } else {
+//         return false;
+//     }
+//   }
+// }
+// ////////////////
+// const loaf = {
+//   flour: 300,
+//   water: 210,
+//   hydration() {
+//     return (this.water / this.flour) * 100;
+//   },
+// }
 
-let input = [
-  {
-    name: 'Tim',
-    status: 'Current student',
-    course: 'Biology'
-  },
-  {
-    name: 'Sue',
-    status: 'Withdrawn',
-    course: 'Mathematics'
-  }
-];
+// console.log(loaf.flour);
+// console.log(loaf.water);
+// console.log(loaf.hydration());
+// ////////////////
+// const object = {
+//   foo: 'hello',
+//   bar: 42,
+//   fum: true,
+//   quux: 'string',
+//   spam: 5
+// }
 
-console.log(enrollInSummerSchool(input));
-
-
-function findById(items,idNum){
-  for(let i = 0;i < items.length; i++){
-    if(items[i].id === idNum){
-      return items[i]
-    }
-  }
-}
-
-function validateKeys(object,expectedKeys){
-  let keys = Object.keys(object);
-for(let i = 0; i < keys.length; i ++){
-  if(keys[i] === expectedKeys[i] && keys.length === expectedKeys.length){
-    return true;
-  }
-else{return false}
-}
-}*/
-
-/*const loaf = {
-  flour: 300,
-  water: 210,
-  hydration() {
-    return (this.water / this.flour) * 100;
-  },
-}
-console.log(loaf.flour);
-console.log(loaf.water);
-console.log(loaf.hydration());*/
-
-const object = {
-  foo: 'hello',
-  bar: 42,
-  fum: true,
-  quux: 'string',
-  spam: 5
-}
-
-for (let key in object) {
-  console.log(`${key}'s value is ${object[key]}`);
-}
+// for (let key in object) {
+//   console.log(`${key}'s value is ${object[key]}`);
+// }
+// ////////////////
