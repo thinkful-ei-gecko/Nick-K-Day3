ea2d396298db9e9401e723f34d53fb3527b04892
NKerner <nickkerner59@gmail.com>
2019-08-14 11:07:06 -0400

    added update object function

--- objectPairedDrills.js
+++ objectPairedDrills.js
@@ -5,3 +5,11 @@ function createMyObject(){
         return 'hello' 
     }
 }
+function updateObject(obj){
+    obj.foo = 'foo';
+    obj.bar ='bar';
+    obj.bizz ='bizz';
+    obj.bang = 'bang';
+    return obj;
+}
+
